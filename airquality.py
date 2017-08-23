73604f08a924f3ebdf8c3603318afcb7e72b879d
Naba-Jeet <31197702+Naba-Jeet@users.noreply.github.com>
2017-08-23 22:21:29 +0530

    Add files via upload

---
+++ airquality.py
@@ -0,0 +1,16 @@
+#(1.) Download the dataset from https://archive.ics.uci.edu/ml/datasets/Air+quality and save .csv file into /data
+import pandas as pd
+import datetime as dt
+def main():
+#(2.) Load the dataset into pandas data frame
+    df=pd.read_excel('data/AirQualityUCI.xlsx')    
+    #(3.) Create a timeseries data frame
+    df = df.rename(columns = {'NO2(GT)':'NO2_GT'})
+    #(4.) Remove all the values of NO2(GT) greater than 100 from march 2005 till june 2005
+    df2 = df[df.Date.dt.year==2005]
+    df2 = df2[df2.Date.dt.month>=3]
+    df2 = df2[df2.Date.dt.month<=6]
+    df2 = df2[df2.NO2_GT>100]
+    df2.to_csv('answer1.csv',sep=',',header=True)    
+#print to file
+main()
